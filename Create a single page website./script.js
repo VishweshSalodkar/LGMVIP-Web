467b77357d8cbc7cde1e5af749bdcd377499088d
VishweshSalodkar <85521059+VishweshSalodkar@users.noreply.github.com>
2021-12-17 00:54:07 +0530

    Add files via upload

---
+++ Create a single page website./script.js
@@ -0,0 +1,10 @@
+function fn1()
+{
+	var userPreference;
+
+if (confirm("Do you want to Submit?") == true) {
+    userPreference = "Submitted Successfully";
+} else {
+    userPreference = "Not Submitted";
+}
+}
